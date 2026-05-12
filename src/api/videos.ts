8419450285cfb1badfcf9f8407f7398be7bff1c1
fbergenholtz23 <s241327@student.dtu.dk>
2026-05-12 20:41:40 +0200

    Update

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -51,15 +51,59 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   let videoKey = `${randomBytes(32).toString("hex")}${ext}`;
   const filePath = `/tmp/${videoId}.mp4`;
   await Bun.write(filePath, videoData);
+  const aspectRatio = await getVideoAspectRatio(filePath);
+  videoKey = `${aspectRatio}/${videoKey}`;
+  const processedVideoPath = await processVideoForFastStart(filePath);
 
   const s3File = cfg.s3Client.file(videoKey, { bucket: cfg.s3Bucket});
-  const localFile = Bun.file(filePath);
+  const localFile = Bun.file(processedVideoPath);
   await s3File.write(localFile, { type: "video/mp4"});
 
   videoMetaData.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${videoKey}`;
   updateVideo(cfg.db, videoMetaData);
 
   await rm(filePath, { force: true });
+  await rm(processedVideoPath, { force: true });
 
   return respondWithJSON(200, null);
+}
+
+export async function getVideoAspectRatio(filepath: string) {
+  const proc = Bun.spawn(["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", filepath], {
+    stderr: "pipe",
+  });
+
+  const stdoutText = await new Response(proc.stdout).text();
+  const stderrText = await new Response(proc.stderr).text();
+
+  if (await proc.exited != 0) {
+    throw new Error("Error with getting video aspect");
+  }
+
+  const width = JSON.parse(stdoutText)["streams"][0]["width"];
+  const height = JSON.parse(stdoutText)["streams"][0]["height"];
+
+  const ratio = width/height;
+
+  if (Math.abs(ratio - 16/9) < 0.1) {
+    return "landscape";
+  } else if (Math.abs(ratio - 9/16) < 0.1) {
+    return "portrait";
+  } else {
+    return "other";
+  }
+}
+
+export async function processVideoForFastStart(inputFilePath: string) {
+  const outputFilePath = `${inputFilePath}.processed`;
+  const proc = Bun.spawn([
+      "ffmpeg",
+    "-i", inputFilePath,
+    "-movflags", "faststart",
+    "-map_metadata", "0",
+    "-codec", "copy",
+    "-f", "mp4",
+    outputFilePath]);
+
+  return outputFilePath;
 }
